e64ae8b053928bb2c2061beb5b9f4fc28175dd97
Fredy Zhang <fredy-zhang@ti.com>
2018-10-15 11:10:55 +0800

    init commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "MP4Encoder.h"
+
+int main(int argc, char** argv)
+{
+	MP4Encoder mp4Encoder;
+    printf("Convert opening !!! \n");
+	// convert H264 file to mp4 file
+	mp4Encoder.WriteH264File("test.264", "test.mp4");
+	printf("Convert completed !!! \n");
+}
